175912d067666a29ab926e24a05cd67449b3b08a
Usama Ahmed <usamabinkashif.98@gmail.com>
2025-01-19 23:06:26 +0500

    user admin flow

---
+++ app/(tabs)/account.tsx
@@ -0,0 +1,13 @@
+import { Text, View, SafeAreaView } from "react-native"
+
+const account = () => {
+  return (
+    <SafeAreaView>
+      <Text>
+        Account Page
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default account
